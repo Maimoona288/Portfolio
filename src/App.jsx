f0d35c5502b8625e8a11ade6c6f30eef0b6d2003
Maimoona288 <maimoonashahbaz348@gmail.com>
2026-04-04 17:41:06 +1200

    Initial project scaffold

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/icons/react.svg'
+import viteLogo from './assets/icons/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className="text-3xl text-blue-500">Working</h1>
+     
+    </>
+  )
+}
+
+export default App
